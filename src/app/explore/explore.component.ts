0d865b774ec260ee6c839c68d9062ce89cb3ec23
sergTesst <sergiycheck@gmail.com>
2021-03-10 00:47:14 +0200

    add explore template

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
-  styleUrls: ['./explore.component.scss']
+  styleUrls: ['./explore.component.scss'],
+  encapsulation:ViewEncapsulation.None
 })
 export class ExploreComponent implements OnInit {
 
